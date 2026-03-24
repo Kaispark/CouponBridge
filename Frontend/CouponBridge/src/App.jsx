af327299274b27abd3af34ae41b434ddaaecdc1b
Kaispark <ac1344470@gmail.com>
2026-03-24 16:17:26 +0530

    Feature: Created Create account page and added route.

--- Frontend/CouponBridge/src/App.jsx
+++ Frontend/CouponBridge/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import Home from './views/home/Home.jsx';
 import Coupons from './views/coupons/Coupons.jsx';
 import CouponDetails from './views/coupon-details/CouponDetails.jsx';
+import SignUp from './views/signup/SignUp.jsx';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -13,6 +15,7 @@ export default function App() {
         <Route path='/' element= {<Home />}/>
         <Route path='/coupons' element= {<Coupons />}/>
         <Route path='/coupons/:id' element={<CouponDetails/>}/>
+        <Route path='/signup' element = { <SignUp/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ Frontend/CouponBridge/src/views/signup/Main.jsx
@@ -0,0 +1,78 @@
+import './Main.css';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import Button from '@mui/material/Button';
+
+const Main = () => {
+  return (
+    <>
+        <div id="signup-main">
+            <div id="signup-left">
+                <div id="signup-left-title" className='section-heading'>Join the fastest growing coupon marketplace</div>
+                <div id="signup-left-subtitle" className='sub-heading'>Start saving or earning today. Create your free account in seconds.</div>
+                <div id="signup-left-sections">
+                    <div className="signup-left-section">
+                        <ShoppingBagIcon sx={{marginBottom: '1rem'}}/>
+                        <div className="signup-left-section-title lg-heading" style={{marginBottom: '1rem'}}>For Buyers</div>
+            
+                        <p className='sub-heading' style={{marginBottom: '0.5rem'}}>Access to verified coupons</p>
+                        <p className='sub-heading' style={{marginBottom: '0.5rem'}}>Secure code delivery</p>
+                        <p className='sub-heading' style={{marginBottom: '0.5rem'}}>buyer protection</p>
+                        <p className='sub-heading' style={{marginBottom: '0.5rem'}}>Save on favourite purchase</p>
+                       
+                    </div>
+                    <div className="signup-left-section">
+                        <StorefrontIcon sx={{marginBottom: '1rem'}}/>
+                        <div className="signup-left-section-title lg-heading" style={{marginBottom: '1rem'}}>For Sellers</div>
+                        
+                        <p className='sub-heading' style={{marginBottom: '0.5rem'}}>Access to verified coupons</p>
+                        <p className='sub-heading' style={{marginBottom: '0.5rem'}}>Secure code delivery</p>
+                        <p className='sub-heading' style={{marginBottom: '0.5rem'}}>buyer protection</p>
+                        <p className='sub-heading' style={{marginBottom: '0.5rem'}}>Save on favourite purchase</p>
+                    
+                    </div>
+                </div>
+            </div>
+            <div id="signup-right">
+                <div id="signup-right-header">
+                    <div id="signup-right-title" className='lg-heading' style={{marginBottom:'1rem'}}>Create your account</div>
+                    <div id="signup-right-subtitle" className='sub-heading'>Choose how you want to use CouponHub</div>
+                </div>
+                
+                <form id='form-section' className='sub-heading'>
+                    <legend  className='heading' style={{marginBottom:'0.5rem'}}>I want to:</legend>
+
+                    <input type="radio" id="customer"  className='radio' name="role"style={{marginBottom:'1rem', marginLeft:'1rem'}}/>
+                    <label for="customer" className='sub-heading' style={ { cursor: 'pointer'} }>Buy Coupons</label><br/>
+
+                    <input type="radio" id="seller" className='radio' name="role" style={{marginBottom:'1rem', marginLeft:'1rem'}}/>
+                    <label for="seller" className='sub-heading' style={ { cursor: 'pointer'}}>Sell Coupons</label><br/>
+
+                    <label for="name"  className='heading' >Full Name</label><br/>
+                    <input type="text" id="name" name="name" className='input' style={{marginBottom:'1rem'}}/><br/>
+                    
+
+                    <label for="email" className='heading'>Email</label><br/>
+                    <input type="email" id="email" className='input' name="email" style={{marginBottom:'1rem'}}/><br/>
+
+                    <label for="password" className='heading'>Password</label><br/>
+                    <input type="password" id="password" className='input' name="password" style={{marginBottom:'1rem'}}/><br/>
+
+                    <label for="conf-password" className='heading'>Confirm Password</label><br/>
+                    <input type="password" id="conf-password" className='input' name="conf-password" style={{marginBottom:'1.5rem'}}/><br/>
+                
+
+                   <Button variant="contained" type='submit' sx= {{ width: '97%', borderRadius: '0.5rem', marginBottom: '0.5rem'}}>Create Account</Button>
+                </form>
+
+                <div id="signup-right-footer" className='sub-heading'>
+                        Already have an account? <Button variant="text">Login here</Button>
+                </div>
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Main
