b6d357735b4908e2a2e30678e3cd2a809f9d192a
ajaykumar0210 <ajaykumar021043@gmail.com>
2026-03-24 10:26:32 +0530

    Clean: Structured components and Added react-router-dom for navigation.

--- Frontend/CouponBridge/src/views/home/Home.jsx
+++ Frontend/CouponBridge/src/views/home/Home.jsx
@@ -1,13 +1,13 @@
-import HeroSection from './HeroSection.jsx';
-import CouponSection from './CouponSection.jsx';
-import ConcludingSection from './ConcludingSection.jsx';
+import Navbar from '../../components/Navbar.jsx';
+import Footer from '../../components/Footer.jsx';
+import Main from './Main.jsx';
 
 function Home() {
     return (
         <>
-            <HeroSection/>
-            <CouponSection/>
-            <ConcludingSection/>
+            <Navbar/>
+            <Main/>
+            <Footer/>
         </>
     )
 }
